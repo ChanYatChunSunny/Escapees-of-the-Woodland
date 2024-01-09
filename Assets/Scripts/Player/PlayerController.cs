20611ef42c585136f298aee97d1145d4d408fc9f
JimmyLam0215 <lamlam200322@gmail.com>
2024-01-09 14:37:30 +0800

    Add player control and try to implement the inventory system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public const int inventorySize = 10;
+    private string[] inventory = new string[inventorySize];
+    public int inverntoryCount = 0;
+    public int health = 100;
+    [SerializeField] float movingSpeed = 5f;
     // Start is called before the first frame update
     public void Start()
     {
@@ -13,6 +18,50 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        
+        PrintFromInventory();
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, movingSpeed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.S)) 
+        {
+            transform.Translate(0, -movingSpeed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(movingSpeed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A)) 
+        {
+            transform.Translate(-movingSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    //function to get the element in inventory
+    public void PrintFromInventory()
+    {
+        for (int i = 0; i < inventorySize; i++) 
+        {
+            Debug.Log(inventory[i] + "\n");
+        }
+    }
+
+    //function to check whether the inventory is full or not
+    public bool CheckInventorySize() 
+    {
+        return inventory.Length <= inventorySize;
+    }
+
+    //function to add elements into inventory
+    public void AddInventory(string element, int count)
+    {
+        if (CheckInventorySize())
+        {
+            inventory[count++] = element;
+        }
+        else
+        {
+            Debug.Log("Inventory is full");
+        }
     }
 }
