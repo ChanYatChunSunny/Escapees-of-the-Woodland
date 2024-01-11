a55609da7edeae699e5fe2cfea00a04f45a7b901
ChanYatChunSunny <124800646+ChanYatChunSunny@users.noreply.github.com>
2024-01-11 20:04:09 +0800

    Lucy and Liam

--- Assets/Scripts/Tiles/LiamTile.cs
+++ Assets/Scripts/Tiles/LiamTile.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class LiamTile : Tile
 {
+    [SerializeField]
+    private GameObject liam;
+    private DialogueGraph dialogueGraph;
     // Start is called before the first frame update
     public override void Start()
     {
-
+        Instantiate(liam, this.transform.position + new Vector3(0.0f, -0.5f, 1), Quaternion.identity);
     }
 
     // Update is called once per frame
     public override void Update()
     {
 
+    }
+    public override void Interact(PlayerController playerController)
+    {
+        playerController.DialogueHandler.dialogueGraph = dialogueGraph;
+        playerController.DialogueHandler.isInConversation = true;
     }
 }
--- Assets/Scripts/Tiles/LucyTile.cs
+++ Assets/Scripts/Tiles/LucyTile.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class LucyTile : Tile
 {
+    [SerializeField]
+    private GameObject lucy;
+    private DialogueGraph dialogueGraph;
     // Start is called before the first frame update
     public override void Start()
     {
-
+        Instantiate(lucy, this.transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     public override void Update()
     {
 
+    }
+    public override void Interact(PlayerController playerController)
+    {
+        playerController.DialogueHandler.dialogueGraph = dialogueGraph;
+        playerController.DialogueHandler.isInConversation = true;
     }
 }
