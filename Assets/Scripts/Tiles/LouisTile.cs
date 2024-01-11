db83d6abfd6df89d53ea8e34bdba223a39b49b4d
ChanYatChunSunny <124800646+ChanYatChunSunny@users.noreply.github.com>
2024-01-11 16:40:38 +0800

    Louis

--- Assets/Scripts/Tiles/LouisTile.cs
+++ Assets/Scripts/Tiles/LouisTile.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class LouisTile : Tile
 {
+
+    [SerializeField]
+    private GameObject louis;
+    private DialogueGraph dialogueGraph;
+
     // Start is called before the first frame update
     public override void Start()
     {
-
+        Instantiate(louis, this.transform.position + new Vector3(0.2f, -0.2f, 1), Quaternion.identity);
     }
 
     // Update is called once per frame
     public override void Update()
     {
 
+    }
+
+    public override void Interact(PlayerController playerController)
+    {
+        playerController.DialogueHandler.dialogueGraph = dialogueGraph;
+        playerController.DialogueHandler.isInConversation = true;
     }
 }
