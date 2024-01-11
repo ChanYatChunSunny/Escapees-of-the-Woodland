5178779bd4ef56cc966f89327a26b2a42843e968
ChanYatChunSunny <124800646+ChanYatChunSunny@users.noreply.github.com>
2024-01-11 17:45:08 +0800

    Annabelle and Elisa

--- Assets/Scripts/Tiles/AnnabelleTile.cs
+++ Assets/Scripts/Tiles/AnnabelleTile.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class AnnabelleTile : Tile
 {
+    [SerializeField]
+    private GameObject annabelle;
+    private DialogueGraph dialogueGraph;
+
     // Start is called before the first frame update
     public override void Start()
     {
-
+        Instantiate(annabelle, this.transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     public override void Update()
     {
 
+    }
+    public override void Interact(PlayerController playerController)
+    {
+        playerController.DialogueHandler.dialogueGraph = dialogueGraph;
+        playerController.DialogueHandler.isInConversation = true;
     }
 }
--- Assets/Scripts/Tiles/ElisaTile.cs
+++ Assets/Scripts/Tiles/ElisaTile.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ElisaTile : Tile
 {
+    [SerializeField]
+    private GameObject elisa;
+    private DialogueGraph dialogueGraph;
     // Start is called before the first frame update
     public override void Start()
     {
-
+        Instantiate(elisa, this.transform.position + new Vector3(0.1f, 0.3f, 1), Quaternion.identity);
     }
 
     // Update is called once per frame
     public override void Update()
     {
 
+    }
+
+    public override void Interact(PlayerController playerController)
+    {
+        playerController.DialogueHandler.dialogueGraph = dialogueGraph;
+        playerController.DialogueHandler.isInConversation = true;
     }
 }
