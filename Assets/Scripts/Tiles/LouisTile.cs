69c506d1d197bd871385df95f948c850a7fdb178
JimmyLam0215 <lamlam200322@gmail.com>
2024-01-13 07:40:38 +0800

    Update LouisTile.cs

--- Assets/Scripts/Tiles/LouisTile.cs
+++ Assets/Scripts/Tiles/LouisTile.cs
@@ -20,7 +20,7 @@ public class LouisTile : Tile
         Dialogue acceptFood = new Dialogue("Yes, please", "Here you go", "player gain 10 health from the food given by Louis", false); 
         Dialogue greetingWithQuestion = new Dialogue("Nice to meet you, You are strong, Why do you got a giant water bottle?", "Because I am a firefighter and love doing sports so I need a lot of water", true);
         Dialogue louisGetMad = new Dialogue("I see. Then you should have a lots of food. Can I get some from you", "What?! (Louis feels like he is being treated as a tool by players)\n(Louis gets angry and don't want to talk to player anymore )", "fail_game");
-        Dialogue stealFood = new Dialogue("You got a lot of food! (Steal his food)", "What are you doing!! (player is punched by the firefighter)", "fail_game");
+        Dialogue stealFood = new Dialogue("You got a lot of food! (Steal his food)", "What are you doing!! (player is punched by the firefighter)", "fail_game_injury");
         Dialogue thank = new Dialogue("Thank you!", "Come and find me if you need help. See you.", "leave_dialogue", true);
         start.Children = new Dialogue[] {greeting, greetingWithQuestion, stealFood};
         greeting.Children = new Dialogue[] {objective, acceptFood};
