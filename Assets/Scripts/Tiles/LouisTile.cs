f27f60f47b7d2c88285097cdc68acc4ae249ec11
JimmyLam0215 <lamlam200322@gmail.com>
2024-01-12 10:33:00 +0800

    Update LouisTile.cs

--- Assets/Scripts/Tiles/LouisTile.cs
+++ Assets/Scripts/Tiles/LouisTile.cs
@@ -13,6 +13,12 @@ public class LouisTile : Tile
     public override void Start()
     {
         Instantiate(louis, this.transform.position + new Vector3(0.2f, -0.2f, 1), Quaternion.identity);
+        Dialogue start = new Dialogue("Hello", "Hi I am Louis, nice to meet you", true);
+        Dialogue greeting = new Dialogue("Nice to meet you! You look so strong!", "Thanks. I used to be a firefighter. Do you need some food? I got a lots", true);
+        Dialogue objective = new Dialogue("Thanks, but no need. I'm here to collect all artifacts, so we can escape from the woodland.", "You mean this one ?", "Louis shows his artifact, the dumbbells", false);
+        Dialogue getArtifact = new Dialogue("Yes!", "Here you go!", "gain_artifact 1");
+
+
     }
 
     // Update is called once per frame
